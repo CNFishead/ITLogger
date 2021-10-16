f709cf387fe859e142dfaab388c351c27b35b100
Austin Howard <cnfishead@hotmail.com>
2021-10-16 13:09:01 -0400

    Logs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 // eslint-disablenextline
 import M from "materialize-css/dist/js/materialize";
 import "./App.css";
+import AppNavbar from "./components/layouts/AppNavbar";
+import Logs from "./components/logs/Logs";
 
 const App = () => {
   useEffect(() => {
     // init materialize JS
     M.AutoInit();
   });
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Fragment>
+        <AppNavbar />
+        <div className="container">
+          <Logs />
+        </div>
+      </Fragment>
+    </div>
+  );
 };
 
 export default App;
